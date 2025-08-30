175e517adebcc44ebbb7e1d979fedc2e086341c3
Heril Shah <heril@MacBook-Air-2.local>
2025-08-30 23:43:25 +0530

    Fix: add h1 and correct imports for ESM

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>React App</h1>   {/* 👈 Added this */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,5 +22,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
